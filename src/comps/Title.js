32f06f421da59ff235a94439a243e3bae244d9ae
Katragadda bhaskar <katragaddabhaskar1729@gmail.com>
2020-08-03 15:42:31 +0530

    initial commit,server running successfully

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>Gallery</h1>
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
